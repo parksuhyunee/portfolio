3ef14d19f29d58ecec8fbb4697d852b56c5e4950
b2uty <b2uty@DESKTOP-LG0RBHJ>
2022-05-20 14:32:30 +0900

    22-05-20

--- portfolio/src/main/webapp/resources/js/login.js
+++ portfolio/src/main/webapp/resources/js/login.js
@@ -7,6 +7,13 @@ $(document).ready(function(){
 		location.href="/member/join";
 	})
 	
+	$("#login").on("click",function(){
+		if($("#id").val()=="" || $("#password").val()==""){
+			alert("빈칸을 채워주세요")
+			return false;
+		}
+	})
+	
 })
 
 
